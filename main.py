c4d2d79a3b654447450c1bcc688dc69b71a4f572
serpentinesr <serpentinesrs@gmail.com>
2024-07-13 09:46:53 +0530

    converted to OOP pattern

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import customtkinter
 from buttons import button_callback
 
-app = customtkinter.CTk()
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-app.title("custom_calc")
-app.geometry("700x500")
+        self.title('custom_calc')
+        self.geometry('700x500')
+        self.grid_columnconfigure((0, 1), weight=1)
 
-button = customtkinter.CTkButton(
-    master=app,
-    text="Hello!",
-    command=button_callback
-)
-button.pack()
+        self.button = customtkinter.CTkButton(
+            master=self,
+            text='Hello!',
+            command=button_callback
+        )
+
+        self.button.grid(
+            row=0,
+            column=0,
+            padx=20,
+            pady=20
+        )
 
 
-
-
-
+app = App()
 app.mainloop()
